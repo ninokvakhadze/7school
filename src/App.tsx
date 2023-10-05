fa053c860a9b3bd9b117d5f39c8cb0861c13e008
ninokvakhadze <ninokvakhadze2@gmail.com>
2023-10-05 22:26:20 +0400

    made header

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import styled, { createGlobalStyle } from "styled-components";
+import Header from "./header-footer/header";
+import GeorgianFont from "./bpg_glaho.ttf";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
+}
+
+export default App;
+
+const GlobalStyles = createGlobalStyle`
+ *{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  }
+  @font-face {
+  src: url(${GeorgianFont});
+  font-family: bpg_ghalo;
+}
+
+`;
---
+++ src/header-footer/header.tsx
@@ -0,0 +1,143 @@
+import styled from "styled-components";
+import Search from "../assets/magnifying-glass-solid.svg";
+import burgerImg from "../assets/bars-solid.svg";
+import logo from "../assets/logo-1.png";
+import { useState } from "react";
+
+function Header() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <WebPageHeader>
+        <HeaderBottom>
+          <Logo src={logo} />
+          <BurgerBar
+            src={burgerImg}
+            onClick={() => {
+              setOpen((prevIsOpen) => !prevIsOpen);
+            }}
+          />
+          <Nav style={open ? { display: "inline" } : { display: "none" }}>
+            <SearchBar type="text" />
+            <UL>
+              <Li>
+                <P>სიახლეები</P>
+              </Li>
+              <Li>
+                <P>სკოლა</P>
+              </Li>
+              <Li>
+                <P>თანამშრომლები</P>
+              </Li>
+              <Li>
+                <P>მოსწავლეები</P>
+              </Li>
+              <Li>
+                <P>შინაგანაწესი</P>
+              </Li>
+              <Li>
+                <P>საჯარო ინფორმაცია</P>
+              </Li>
+              <Li>
+                <P>კონტაქტი</P>
+              </Li>
+            </UL>
+          </Nav>
+        </HeaderBottom>
+      </WebPageHeader>
+    </>
+  );
+}
+
+export default Header;
+
+const WebPageHeader = styled.header``;
+const SearchBar = styled.input`
+  /* position: fixed; */
+  width: 100%;
+  outline: none;
+  border: none;
+  padding: 7px 10px;
+  background-color: red;
+  background-image: url(${Search});
+  background-size: 20px;
+  background-repeat: no-repeat;
+  background-position: center right 12px;
+  color: #fff;
+  @media only screen and (min-width: 768px) {
+    position: absolute;
+    top: 0;
+    right: 20px;
+    width: 500px;
+  }
+`;
+const HeaderBottom = styled.div`
+  padding: 15px 10px;
+  background-color: blue;
+  display: flex;
+  justify-content: space-between;
+  @media only screen and (min-width: 768px) {
+    padding: 30px 20px;
+  }
+`;
+const BurgerBar = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-top: 10px;
+  @media only screen and (min-width: 768px) {
+    width: 30px;
+    height: 30px;
+    margin-top: 15px;
+  }
+  @media only screen and (min-width: 1100px) {
+    display: none;
+  }
+`;
+const Logo = styled.img`
+  height: 40px;
+  width: 120px;
+  @media only screen and (min-width: 768px) {
+    width: 160px;
+    height: 60px;
+  }
+`;
+const Nav = styled.nav`
+  background-color: green;
+  position: absolute;
+  left: 0;
+  top: 70px;
+  width: 100%;
+  @media only screen and (min-width: 768px) {
+    top: 120px;
+  }
+  @media only screen and (min-width: 1100px) {
+    position: unset;
+    padding: 0;
+    background-color: unset;
+  }
+`;
+const UL = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  padding: 40px;
+  @media only screen and (min-width: 1100px) {
+    flex-direction: row;
+    gap: 20px;
+    justify-content: right;
+    padding: 0;
+    margin-top: 20px;
+  }
+`;
+const Li = styled.li``;
+const P = styled.p`
+  text-align: center;
+  font-family: bpg_ghalo;
+  color: #ffffff;
+  font-size: 19px;
+  line-height: 20px;
+  font-weight: 600;
+  text-align: center;
+`;
